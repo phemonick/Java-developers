a96b67ea4898e1b1ce0b2c5cb303792eda5498a0
Oluwafemi <phemy.smith@gmail.com>
2018-11-05 12:37:38 +0100

    ft(launchDetailActivity): Launch detailActivity from main activity
    - create layout file for detailActivity
    - Launch detail activity from main activity
    
    [Finishes #155820624]

--- app/src/main/java/com/example/adekunleoluwafemi/javadevelopers/MainActivity.java
+++ app/src/main/java/com/example/adekunleoluwafemi/javadevelopers/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.adekunleoluwafemi.javadevelopers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mUserList;
+    private Button nextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mUserList = findViewById(R.id.my_recycler_view);
+        nextPage = findViewById(R.id.to_detail);
+    }
+
+    public void toNextPage(View view) {
+        System.out.println("hello clicked");
+        startActivity(new Intent(this, DetailActivity.class));
     }
 }
