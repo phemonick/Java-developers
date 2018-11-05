8934e064c17580d751beeade80ff9ca74b6744f7
Oluwafemi <phemy.smith@gmail.com>
2018-11-05 15:47:22 +0100

    ft(modelClasses): Implement user model
    - add GithubUsersResponse to model package
    - add GithubUsers to model package
    
    [Finishes #155889952]

--- app/src/main/java/com/example/adekunleoluwafemi/javadevelopers/model/GithubUsersResponse.java
+++ app/src/main/java/com/example/adekunleoluwafemi/javadevelopers/model/GithubUsersResponse.java
@@ -1,7 +1,6 @@
 package com.example.adekunleoluwafemi.javadevelopers.model;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -11,19 +10,12 @@ import java.util.List;
 public class GithubUsersResponse {
     @SerializedName("items")
     @Expose
-//    List of type GithubUsers from model
-    private List<GithubUsers> githubUsers;
+    private List<GithubUsers> githubUsers ;
 
-//    To avoid null pointer exceptions from trying to get back the user lists,
-    public GithubUsersResponse() {
+    GithubUsersResponse() {
         githubUsers = new ArrayList<GithubUsers>();
     }
 
-    public static GithubUsersResponse parseJSON(String response) {
-        Gson gson = new GsonBuilder().create();
-        GithubUsersResponse githubUsersResponse = gson.fromJson(response, GithubUsersResponse.class);
-        return githubUsersResponse;
-    }
 
 //    returns List of type GithubUsers
     public List<GithubUsers> getGithubUsers() {
