2bb8d63917d0d526a48f0f032a5bf27e19d5f810
Oluwaphemy Adekunle <phemy.smith@gmail.com>
2018-11-07 12:09:41 +0100

    ft(connectRecyclerView): Connect recyclerView activity (#4)
    - add recyclerView instance to onCreate
    
    [Finishes #155768857]

--- app/src/main/java/com/example/adekunleoluwafemi/javadevelopers/MainActivity.java
+++ app/src/main/java/com/example/adekunleoluwafemi/javadevelopers/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.adekunleoluwafemi.javadevelopers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mUserList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUserList = findViewById(R.id.my_recycler_view);
     }
 }
