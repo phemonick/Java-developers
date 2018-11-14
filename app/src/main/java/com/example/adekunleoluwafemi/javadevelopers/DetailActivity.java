0b7c3ab4536f8eb2f19843e20ba6db9d214a6b7d
Oluwafemi <phemy.smith@gmail.com>
2018-11-14 14:48:15 +0100

    ft(share): Implement share
    - add menu layout to xml
    - implement share intent
    
    [Finishes #156008359]

--- app/src/main/java/com/example/adekunleoluwafemi/javadevelopers/DetailActivity.java
+++ app/src/main/java/com/example/adekunleoluwafemi/javadevelopers/DetailActivity.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -18,6 +23,7 @@ public class DetailActivity extends AppCompatActivity {
     private static final String GITHUB_LINK = "com.example.adekunleoluwafemi.githubLink";
     private static final String IS_ADMIN = "com.example.adekunleoluwafemi.isAdmin";
     private static final String USER_ID = "com.example.adekunleoluwafemi.userId";
+    public ShareActionProvider mShareActionProvider;
 
     private CircleImageView mUserImage;
     TextView userName;
@@ -48,6 +54,27 @@ public class DetailActivity extends AppCompatActivity {
         githubLink.setText(link);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        Intent i = new Intent(android.content.Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_TEXT, "Checkout this awesome developer "
+                + getIntent().getStringExtra(GITHUB_USERNAME) + " " + getIntent().getStringExtra(GITHUB_LINK));
+        mShareActionProvider.setShareIntent(i);
+        // Return true to display menu
+        return true;
+    }
+
 
     public static Intent newIntent(Context packageContext, boolean admin, String id,
                                    String userImage, String userName, String githubLink){
